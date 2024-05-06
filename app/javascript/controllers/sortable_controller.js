f3629b3fd3b2a87431ea8f4d6eb39896ae892880
Matt Marini <mattmarini8765@gmail.com>
2024-05-07 08:07:58 +1000

    Can move cards between columns

--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -4,7 +4,19 @@ import Sortable from 'sortablejs';
 // Connects to data-controller="sortable"
 export default class extends Controller {
   connect() {
-    Sortable.create(this.element, { /* options */ });
+    //Sortable.create(this.element, { /* options */ });
+    const initKanbanSortable = (ulElements) => {
+      ulElements.forEach((ul) => {
+        new Sortable(ul, {
+            group: 'kanban', // set both lists to same group
+            animation: 300
+        });
+      });
+    };
+
+    const kanbanUls = document.querySelectorAll(".kanban .kanban-col");
+    if (kanbanUls) {
+      initKanbanSortable(kanbanUls);
+      };
   }
-  
 }
