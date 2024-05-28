3d900f28d6dde019c0f0ceb525851dff7f2b155e
Matt Marini <mattmarini8765@gmail.com>
2024-05-29 08:17:46 +1000

    Make cards for columns functionality complete

--- app/controllers/kanban_columns_controller.rb
+++ app/controllers/kanban_columns_controller.rb
@@ -1,9 +1,35 @@
 class KanbanColumnsController < ApplicationController
 
+  def edit
+    @column = KanbanColumn.find(params[:id])
+    @column.cards.build
+  end
+
   def sort
     @column = KanbanColumn.find(params[:id])
     @column.update(row_order_position: params[:row_order_position])
     head :no_content
+  end
+
+  # PATCH/PUT /column/1 or /column/1.json
+  def update
+    @column = KanbanColumn.find(params[:id])
+    respond_to do |format|
+      if @column.update(kanban_column_params)
+        format.html { redirect_to kanban_board_url(@column.kanban_board_id), notice: "Card was successfully updated." }
+        format.json { render :show, status: :ok, location: @column }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @column.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  # Currently only makes cards, may need to update it eventually
+  def kanban_column_params
+    params.require(:kanban_column).permit(cards_attributes: [:name, :start_date, :end_date, :due_date, :kanban_column_id, :_destroy])
   end
 
 end
--- app/models/kanban_column.rb
+++ app/models/kanban_column.rb
@@ -1,8 +1,10 @@
 class KanbanColumn < ApplicationRecord
-  has_many :cards
+  has_many :cards, dependent: :destroy
   belongs_to :kanban_board
 
   # from the ranked model gem https://github.com/brendon/ranked-model
   include RankedModel
   ranks :row_order
+  accepts_nested_attributes_for :cards, reject_if: :all_blank, allow_destroy: true
+
 end
