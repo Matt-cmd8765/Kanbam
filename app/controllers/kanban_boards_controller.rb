cdac7be373ff8aa204fc8f54b185261f91be2326
Matt Marini <mattmarini8765@gmail.com>
2024-05-30 22:34:37 +1000

    Can add columns in already made board

--- app/controllers/kanban_boards_controller.rb
+++ app/controllers/kanban_boards_controller.rb
@@ -39,10 +39,31 @@ class KanbanBoardsController < ApplicationController
     end
   end
 
+  def edit
+    @board = KanbanBoard.find(params[:id])    
+  end
+
+  def update
+    @board = KanbanBoard.find(params[:id])
+    respond_to do |format|
+      if @board.update(kanban_update_column_params)
+        format.html { redirect_to kanban_board_url(@board.id), notice: "Column was successfully created." }
+        format.json { render :show, status: :ok, location: @column }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @column.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
   def kanban_board_params
     params.require(:kanban_board).permit(:name, :user_id, kanban_columns_attributes: [:name, :_destroy])
+  end
+
+  def kanban_update_column_params
+    params.require(:kanban_board).permit(kanban_columns_attributes: [:name, :kanban_board_id, :_destroy])
   end
 
 # class end
