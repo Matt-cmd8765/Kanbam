fa92ec451f8789b711dcd4d22e948a8bb2524bd4
Matt Marini <mattmarini8765@gmail.com>
2024-06-12 17:32:30 +1000

    Make sitmulus controllers for reply buttons

---
+++ app/javascript/controllers/hide_controller.js
@@ -0,0 +1,33 @@
+import { Controller } from "@hotwired/stimulus"
+
+// OK so this controller is for buttons to hide / unhide the reply form. To start the reply form and discard buttons are hidden. Then clicking reply unhides them and hides the reply button. The discard button then rehides the form and reply buttons. 
+
+//Is it the best way to do this? No! But just don't ask questions ok?
+
+export default class extends Controller {
+  static targets = ["reply", "button", "discardbutton"]
+
+  hide() {
+    this.replyTargets.forEach(element => {
+        element.hidden = true
+    });
+    this.discardbuttonTargets.forEach(element => {
+        element.hidden = true
+    });
+    this.buttonTargets.forEach(element => {
+        element.hidden = false
+    });
+  }
+
+  unhide() {
+    this.replyTargets.forEach(element =>{
+        element.hidden = false
+    });
+    this.buttonTargets.forEach(element => {
+        element.hidden = true
+    });
+    this.discardbuttonTargets.forEach(element => {
+        element.hidden = false
+    });
+  }
+}
